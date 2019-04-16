2e57db94ee53db854af16d9e53bd2a20d97896e7
Sang Vo <sangvo111@gmail.com>
2019-04-16 15:46:22 +0700

    Start vuex for project

---
+++ resources/js/admin/vuex/index.js
@@ -0,0 +1,18 @@
+// Import Vue
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+// Import module
+import state from './state'
+import mutations from './mutations'
+import actions from './actions'
+import getters from './getters'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state,
+  mutations,
+  actions,
+  getters
+})
---
+++ resources/js/config.js
@@ -0,0 +1,6 @@
+// Define route API
+var location = window.location
+var apiUrl = location.protocol + '//' + location.host + '/api'
+export const APP_CONFIG = {
+  API_URL: apiUrl
+}
