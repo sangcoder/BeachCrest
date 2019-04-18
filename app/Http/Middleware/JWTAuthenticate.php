03fce52c28b678066b11b0465ce74e38f90fc56f
Sang Vo <sangvo111@gmail.com>
2019-04-18 23:05:48 +0700

    Config JwtAuth, middleware, provider

---
+++ app/Http/Middleware/JWTAuthenticate.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Tymon\JWTAuth\Token;
+use Tymon\JWTAuth\JWTAuth;
+use Illuminate\Support\Facades\Auth;
+
+class JWTAuthenticate
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        // Middleware decode token từ front end JWT
+        $rawToken = $request->cookie('token'); // Nhận token từ cookie front end
+        $token = new Token($rawToken);
+        $payload = JWTAuth::decode($token);
+        Auth::loginUsingId($payload['sub']); // Get ID and Login
+        // pass 
+        return $next($request);
+
+    }
+}
--- app/User.php
+++ app/User.php
@@ -39,4 +39,25 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // Để sử dụng được JWT Auth
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
